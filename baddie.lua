ad34cc5a1191ca6fa9803a6ab7f42991824925c6
Candorada <51519091+Candorada@users.noreply.github.com>
2026-02-18 06:43:30 +0100

    Update baddie.lua

--- baddie.lua
+++ baddie.lua
@@ -1,11 +1,14 @@
 if not game:IsLoaded() then game.Loaded:Wait() end
-if game.PlaceId ~= 79305036070450 then return end
+--if game.PlaceId ~= 79305036070450 then return end
 while game:GetService("Players").LocalPlayer == nil do task.wait() end --prevents execute failure on :GetMouse()
 
 
+local root = "https://raw.githubusercontent.com/Candorada/r/refs/heads/main/"
+local randomRejoin = loadstring(game:HttpGet(root.."randomRejoin.lua"))()
+
 game.CoreGui.RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(c)
     if(c.Name == "ErrorPrompt") then
-        queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/Candorada/r/refs/heads/main/baddie.lua"))()')
+        queue_on_teleport('loadstring(game:HttpGet("'..root..'baddie.lua"))()')
         randomRejoin()
     end
 end)
@@ -84,13 +87,6 @@ function buyEgg(name,count)
     local ohNumber2 = count
     game:GetService("ReplicatedStorage").Events.RegularPet:InvokeServer(ohString1, ohNumber2)
 end
-local TeleportService = game:GetService("TeleportService")
-function tp(place,job,plr)
-local success, errorMessage,rv = pcall(function()
-return TeleportService:TeleportToPlaceInstance(place, job , plr)
-end) 
-return success;
-end
 
 function buyAll()
     repeat task.wait() until game:GetService("Players").LocalPlayer
@@ -110,51 +106,6 @@ function buyAll()
             game:GetService("ReplicatedStorage").Events.buy:InvokeServer(i, v, "potion")
         end
     end
-end
-
-local randomRejoin;randomRejoin=function()
-    local p = game:GetService("Players").LocalPlayer
-    local to = Instance.new("TeleportOptions")
-
-    local http = game:GetService("HttpService")
-    local headers = {
-        ["Content-Type"] = "application/json"
-    }
-    local data = {
-        ["content"] = message
-    }
-    local body = http:JSONEncode(data)
-    local url = "https://games.roblox.com/v1/games/"..tostring(game.PlaceId).."/servers/Public?sortOrder=Asc&limit=100"
-    local response = request({
-        Url = url,
-        Method = "GET",
-        Headers = headers,
-        Body = body
-    })
-    local connect;
-    connect = TeleportService.TeleportInitFailed:Connect(function(player, result, errorMessage, placeId, jobId)
-        connect:Disconnect()
-        task.wait(1)
-        randomRejoin()
-    end)
-    if response.Success then
-        writefile("baddieWebRequestLoggs.json",response.Body)
-        local json = http:JSONDecode(response.Body)
-        local data = json.data
-        local id = data[math.ceil(math.random()*#data)].id
-        if not tp(game.PlaceId,id , p) then
-            randomRejoin()
-        end
-    else
-        local json = http:JSONDecode(readfile("baddieWebRequestLoggs.json"))
-        local data = json.data
-        local id = data[math.ceil(math.random()*#data)].id
-        if not tp(game.PlaceId,id, p) then
-            randomRejoin()
-        end
-    end
-    --connect:Disconnect()
-    --https://games.roblox.com/v1/games/79305036070450/servers/Public?sortOrder=Desc&limit=100
 end
 
 function nullityExists() 
