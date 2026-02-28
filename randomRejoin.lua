5733f9caa8f8f67ee4bed77901d65a0b4c35cd24
Candorada <51519091+Candorada@users.noreply.github.com>
2026-02-28 23:38:26 +0100

    Update randomRejoin.lua

--- randomRejoin.lua
+++ randomRejoin.lua
@@ -7,8 +7,8 @@ function tp(place,job,plr)
 end
 
 getgenv().isTeleporting = getgenv().isTeleporting and true or false 
-local randomRejoin;randomRejoin=function(recursiveCall)
-    if(getgenv().isTeleporting and (recursiveCall ==nil or recursiveCall == false)) then return end 
+local randomRejoin;randomRejoin=function()
+    if(getgenv().isTeleporting) then return end 
     getgenv().isTeleporting = true
     local p = game:GetService("Players").LocalPlayer
     local http = game:GetService("HttpService")
@@ -28,11 +28,11 @@ local randomRejoin;randomRejoin=function(recursiveCall)
     })
     local connect;
     connect = TeleportService.TeleportInitFailed:Connect(function(player, result, errorMessage, placeId, jobId)
-        --getgenv().isTeleporting = false --this should stay away since randomrejoin down there will automatically set it as true again
         connect:Disconnect()
+        getgenv().isTeleporting = false
         connect = nil;
         task.wait(1)
-        randomRejoin(true)
+        randomRejoin()
     end)
     local json;
     if response.Success then
@@ -48,8 +48,9 @@ local randomRejoin;randomRejoin=function(recursiveCall)
         local id = data[math.ceil(math.random()*#data)].id
         if not tp(game.PlaceId,id , p) then
             if connect then connect:Disconnect(); connect = nil end
+            getgenv().isTeleporting = false
             task.wait(1)
-            randomRejoin(true)
+            randomRejoin()
         else
            if connect then connect:Disconnect(); connect = nil end --cleans up memory
         end
